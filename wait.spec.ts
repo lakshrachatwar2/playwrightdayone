bb9308f49d5fd34857e3caf424647486fa5cb95b
Laxmikant <lakshrachatwar2@gmail.com>
2026-08-22 16:46:03 +0530

    waiting mechanism file two

--- wait.spec.ts
+++ wait.spec.ts
@@ -7,3 +7,17 @@ test("wait mechanism",async({page})=>{
     await page.locator("//button[text()='Dropdown']").click();
     await page.locator("//a[text()='Flipkart123']").click();
 });
+
+test("perticular element waiting mechanism", async({page})=>{
+
+    await page.goto("https://omayo.blogspot.com/");
+    await page.locator("//button[text()='Dropdown']").click();
+    await page.locator("//a[text()='Flipkart']").click({timeout:1000});
+})
+
+test("wait till load",async({page})=>{
+
+    await page.goto("https://omayo.blogspot.com/");
+    await page.locator("//a[text()='jqueryui']").click();
+    await page.waitForLoadState();
+});
